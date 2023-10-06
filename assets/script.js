be5b1e4ad28998511e39f290f2a3ef1f3f9aa081
13010-34-08 <13010-34-08@exchange.ad.afpanet>
2023-10-06 11:53:41 +0200

    feature/menu burger

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const navEl = document.querySelector('.');
+const hamburgerEl = document.querySelector('.hamburger');
+
+hamburgerE1.addEventListener('click', ()=> {
+    navEl.classList.add("nav--open");
+});
